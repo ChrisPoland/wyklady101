5751d4d56dd461d0fa6cf4ab6689bfefb0ff5297
Olaf Górski <grski1337@gmail.com>
2017-01-05 14:10:13 +0000

    lekcja2

---
+++ kod/lekcja2/Lekcja2.java
@@ -0,0 +1,9 @@
+public class Lekcja2{
+    public static void main(String[] args){
+    if (args.length != 0)
+    {
+        System.out.println("HELLO "+args[0]);
+    }
+    else System.out.println("Hello world!");
+    }
+}
